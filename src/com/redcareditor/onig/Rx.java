7d91658b3b3260a1dcaa6995f0fff9102dc895c5
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-10 15:57:12 +0200

    switched to line-by-line-parsing. and changed the return values of the search function. now were finally seeing a match...

--- src/com/redcareditor/onig/Rx.java
+++ src/com/redcareditor/onig/Rx.java
@@ -2,7 +2,7 @@ package com.redcareditor.onig;
 
 import java.io.UnsupportedEncodingException;
 
-import org.jcodings.specific.ASCIIEncoding;
+import org.jcodings.specific.UTF8Encoding;
 import org.joni.Matcher;
 import org.joni.Option;
 import org.joni.Regex;
@@ -31,9 +31,14 @@ public class Rx {
 	public Match search(String target, int start, int end) {
 		byte[] bytes;
 		try {
-			bytes = target.getBytes("iso-8859-2");
+			bytes = target.getBytes("UTF-8");
 			Matcher matcher = regex.matcher(bytes, 0, bytes.length);
 			int a = matcher.search(0, bytes.length, Option.NONE);
+			
+			if(a == -1){
+				return null;
+			}
+			
 			Region region = matcher.getEagerRegion();
 			return new Match(regex, region, target);
 
@@ -46,9 +51,9 @@ public class Rx {
 	public Regex compileRegex(String pattern) {
 		byte[] bytes;
 		try {
-			bytes = pattern.getBytes("iso-8859-2");
+			bytes = pattern.getBytes("UTF-8");
 			return new Regex(bytes, 0, bytes.length, Option.DEFAULT,
-					ASCIIEncoding.INSTANCE, Syntax.DEFAULT);
+					UTF8Encoding.INSTANCE, Syntax.RUBY);
 
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
--- test/com/redcareditor/onig/RxTest.java
+++ test/com/redcareditor/onig/RxTest.java
@@ -1,6 +1,10 @@
 package com.redcareditor.onig;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -9,18 +13,28 @@ import com.redcareditor.util.FileUtility;
 public class RxTest {
 	@Test
 	public void testSingleFoo() {
-		String pattern = "((class|module|def|end) \\w+ < )+";
-		String target = "";
+		String pattern = "^\\s*(class)\\s+(([.a-zA-Z0-9_:]+(\\s*(&lt;)\\s*[.a-zA-Z0-9_:]+)?)|((&lt;&lt;)\\s*[.a-zA-Z0-9_:]+))+";
+		String fileContents = "";
 		try {
-			target = new String(FileUtility.readFully("input/autocompleter.rb"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		Rx regex = new Rx(pattern);
-		Match m = regex.search(target, 0, target.length());
+			fileContents = new String(FileUtility.readFully("input/autocompleter.rb"));
 
-		for (Range r : m) {
-			System.out.println(r);
+			BufferedReader reader = new BufferedReader(new StringReader(fileContents));
+
+			Rx regex = new Rx(pattern);
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				Match m = regex.search(line, 0, line.length());
+				
+				if (m != null) {
+					System.out.println(line);
+					for (Range r : m) {
+						System.out.println(r);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
