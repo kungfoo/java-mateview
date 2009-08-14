632a6bd68f9e53ff313b0f40359731c68752d981
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-14 20:50:52 +0200

    done with textLocation.

--- test/com/redcareditor/mate/TextLocationTest.java
+++ test/com/redcareditor/mate/TextLocationTest.java
@@ -20,6 +20,9 @@ public class TextLocationTest {
 		TextLocation l3 = new TextLocation(10, 11);
 		assertFalse(l3.equals(l1));
 		assertFalse(l2.equals(l3));
+		
+		Integer i = new Integer(12);
+		assertFalse(l1.equals(i));
 	}
 	
 	@Test
@@ -35,6 +38,12 @@ public class TextLocationTest {
 	
 	@Test
 	public void testBigerThan(){
+		TextLocation l1 = new TextLocation(10,10);
+		TextLocation l2 = new TextLocation(9, 10);
 		
+		assertEquals(1, l1.compareTo(l2));
+		
+		TextLocation l3 = new TextLocation(10, 3);
+		assertEquals(1, l1.compareTo(l3));
 	}
 }
