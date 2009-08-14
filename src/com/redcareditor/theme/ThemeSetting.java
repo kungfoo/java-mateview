06e0207572efcbb09cafc09b53e2d0cd2e34eddc
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-14 21:23:23 +0200

    added theme manager and started stubbing out theme related stuff.

---
+++ src/com/redcareditor/theme/ThemeSetting.java
@@ -0,0 +1,18 @@
+package com.redcareditor.theme;
+
+import java.util.List;
+import java.util.Map;
+
+import org.joni.Matcher;
+
+public class ThemeSetting {
+	public String name;
+	public String selector;
+	public Map<String, String> settings;
+	public List<Matcher	> matchers;
+	
+	
+	public ThemeSetting(){
+		
+	}
+}
