f6ddda87186af6ac044de2ddf77664034d6f71b4
Silvio Heuberger <kungfoo@kungfoo-desktop.(none)>
2009-08-15 20:20:56 +0200

    added theme for testing.

--- src/com/redcareditor/theme/Theme.java
+++ src/com/redcareditor/theme/Theme.java
@@ -1,6 +1,33 @@
 package com.redcareditor.theme;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.redcareditor.plist.Dict;
+import com.redcareditor.plist.PlistPropertyLoader;
+
 
 public class Theme {
+	public String author;
+	public String name;
+	public Map<String, String> globalSettings = new HashMap<String, String>();
+	public List<ThemeSetting> settings = new ArrayList<ThemeSetting>();
+	public Map<String, ThemeSetting> cachedSettingsForScopes = new HashMap<String, ThemeSetting>();
 	
+	// stuff only needed in this class
+	private boolean initialized = false;
+	private PlistPropertyLoader propertyLoader;
+	
+	public Theme(Dict dict){
+		propertyLoader = new PlistPropertyLoader(dict, this);
+		
+		propertyLoader.loadStringProperty("name");
+		propertyLoader.loadStringProperty("author");
+		
+		
+		
+		
+	}
 }
---
+++ test/com/redcareditor/theme/RailsCastThemeTest.java
@@ -0,0 +1,22 @@
+package com.redcareditor.theme;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class RailsCastThemeTest {
+	@Before
+	public void setup(){
+		
+	}
+	
+	@Test
+	public void testSingleScopeSetting(){
+		
+	}
+	
+	@Test
+	public void testMultipleScopeSetting(){
+		
+	}
+}
