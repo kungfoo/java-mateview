f9e989c67320fd77dba74854b8281bea26db3275
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-17 06:05:21 +0100

    Don't import Java class more than once

--- lib/java-mateview.rb
+++ lib/java-mateview.rb
@@ -3,18 +3,21 @@ $CLASSPATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "bin"))
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "jdom"))
 
-class JavaMateView
-  import com.redcareditor.mate.Grammar
-  import com.redcareditor.mate.TextLocation
-end
+unless defined?(JavaMateView)
 
-class Plist
-  import com.redcareditor.plist.Dict
-  import com.redcareditor.plist.PlistNode
-  import com.redcareditor.plist.PlistPropertyLoader
-end
+  class JavaMateView
+    import com.redcareditor.mate.Grammar
+    import com.redcareditor.mate.TextLocation
+  end
 
-class Onig
-  import com.redcareditor.onig.Rx
-  import com.redcareditor.onig.Match
+  class Plist
+    import com.redcareditor.plist.Dict
+    import com.redcareditor.plist.PlistNode
+    import com.redcareditor.plist.PlistPropertyLoader
+  end
+
+  class Onig
+    import com.redcareditor.onig.Rx
+    import com.redcareditor.onig.Match
+  end
 end
