5914d4179d3119a4dd94f6d1693daa5651ce27f2
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-18 11:02:34 +0200

    fixed suspected bug by fixing the spec test.

--- spec/theme/theme_spec.rb
+++ spec/theme/theme_spec.rb
@@ -3,13 +3,12 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper.rb")
 describe JavaMateView::Theme do
   describe "it has global settings" do
     before(:all) do
-      dict = Plist::Dict.parseFile("spec/fixtures/Railscasts.tmTheme")
-      p dict.getString("name")
-      theme = JavaMateView::Theme.new(theme)
+      dict = Plist::Dict.parse_file("spec/fixtures/Railscasts.tmTheme")
+      @theme = JavaMateView::Theme.new(dict)
     end
     
     it "sets the background" do
-      theme.globalSettings.get("background").should == "#2B2B2B"
+      @theme.globalSettings.get("background").should == "#2B2B2B"
     end
   end
 end
