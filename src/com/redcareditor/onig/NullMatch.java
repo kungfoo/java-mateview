d647c706dc0d5faaaeb273440ec1dc84f75920f5
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-24 11:55:27 +0200

    wax

---
+++ src/com/redcareditor/onig/NullMatch.java
@@ -0,0 +1,10 @@
+package com.redcareditor.onig;
+
+public class NullMatch extends Match{
+	public NullMatch() {
+		super(null,null,null);
+	}
+	
+	//TODO: overwrite methods to make this class behave where Match will be used.
+	
+}
