6dbb4b0db28154799bd275fdaaa90857769dcd4d
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-24 20:57:36 +0100

    Added ScopeMatcher and failing tests

---
+++ src/com/redcareditor/mate/ScopeMatcher.java
@@ -0,0 +1,14 @@
+package com.redcareditor.mate;
+
+import java.util.ArrayList;
+
+import com.redcareditor.onig.Rx;
+
+public class ScopeMatcher {
+	public Rx pos_rx;
+	public ArrayList<Rx> neg_rxs;
+
+	public static boolean testMatch(String a, String b) {
+		return true;
+	}
+}
