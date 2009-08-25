1e524dbee62f98133e4687abe494ed72e7a28d47
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-25 20:00:57 +0100

    Created MateText class as subclass of StyledText

---
+++ src/com/redcareditor/mate/MateText.java
@@ -0,0 +1,10 @@
+package com.redcareditor.mate;
+
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Composite;
+
+public class MateText extends StyledText {
+	public MateText(Composite composite, int style) {
+		super(composite, style);
+	}
+}
