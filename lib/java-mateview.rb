836570da4cd93d1da55aa8f52d1de864e61d1a36
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-25 20:24:17 +0100

    Created swt_wrapper.rb and started making an example jruby app to use MateText

--- lib/java-mateview.rb
+++ lib/java-mateview.rb
@@ -1,11 +1,16 @@
+require 'java'
 
 $CLASSPATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "bin"))
+$:.push(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")))
 
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "jdom"))
+require 'jdom'
+require 'swt'
+require 'swt_wrapper'
 
 unless defined?(JavaMateView)
 
   class JavaMateView
+    import com.redcareditor.mate.MateText
     import com.redcareditor.mate.Grammar
     import com.redcareditor.mate.TextLocation
     import com.redcareditor.theme.Theme
@@ -22,3 +27,58 @@ unless defined?(JavaMateView)
     import com.redcareditor.onig.Match
   end
 end
+
+class MateExample
+  def initialize
+    display = Swt::Widgets::Display.new
+    @shell = Swt::Widgets::Shell.new(display)
+
+    build_application_menu
+    build_styled_text
+    setup_listeners
+
+    @shell.pack
+    @shell.open
+    until @shell.disposed?
+      unless display.readAndDispatch
+        display.sleep
+      end
+    end
+    display.dispose
+  end
+  
+  def build_application_menu
+    @menu = Swt::Widgets::Menu.new(@shell, Swt::SWT::BAR)
+		build_file_menu
+		build_edit_menu
+		@shell.set_menu_bar(@menu)
+  end
+  
+  def build_file_menu
+  	file_header = Swt::Widgets::MenuItem.new(@menu, Swt::SWT::CASCADE)
+  	file_header.set_text("&File")
+	
+  	file = Swt::Widgets::Menu.new(@shell, Swt::SWT::DROP_DOWN)
+  	file_header.set_menu(file)
+	
+  	open = Swt::Widgets::MenuItem.new(file, Swt::SWT::PUSH)
+  	open.set_text("Open")
+	
+  	close = Swt::Widgets::MenuItem.new(file, Swt::SWT::PUSH)
+  	close.set_text("Close")
+  end
+  
+  def build_styled_text
+  end
+  
+  def setup_listeners
+  end
+  
+  def build_edit_menu
+  end
+end
+
+
+MateExample.new
+
+
---
+++ lib/swt_wrapper.rb
@@ -0,0 +1,13 @@
+
+
+module Swt
+  import org.eclipse.swt.SWT
+  
+  module Widgets
+    import org.eclipse.swt.widgets.Display
+    import org.eclipse.swt.widgets.Shell
+    import org.eclipse.swt.widgets.Menu
+    import org.eclipse.swt.widgets.MenuItem
+    
+  end
+end
