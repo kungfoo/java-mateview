0cde6029a93c63521fefc932e619048096347bd0
aokai <sohrauer@googlemail.com>
2009-08-25 17:35:52 +0200

    Change Range and Test

--- test/com/redcareditor/mate/FilledRangeSetTest.java
+++ test/com/redcareditor/mate/FilledRangeSetTest.java
@@ -32,6 +32,7 @@ public class FilledRangeSetTest {
 	@Test
 	public void testShouldMergeRanges() {
 		rs.add(14, 16);
+		assertEquals(rs.length(), 3);
 		assertEquals(rs.get(0), new Range(1,3));
 		assertEquals(rs.get(1), new Range(5,5));
 		assertEquals(rs.get(2), new Range(10,16));
@@ -40,6 +41,7 @@ public class FilledRangeSetTest {
 	@Test
 	public void testShouldMergeRanges2() {
 		rs.add(7, 11);
+		assertEquals(rs.length(), 3);
 		assertEquals(rs.get(0), new Range(1,3));
 		assertEquals(rs.get(1), new Range(5,5));
 		assertEquals(rs.get(2), new Range(7,15));
@@ -48,18 +50,21 @@ public class FilledRangeSetTest {
 	@Test
 	public void testShouldMergeTwoRanges() {
 		rs.add(4, 11);
+		assertEquals(rs.length(), 1);
 		assertEquals(rs.get(0), new Range(1,15));
 	}
 
 	@Test
 	public void testShouldMergeAllRanges() {
 		rs.add(1, 20);
+		assertEquals(rs.length(), 1);
 		assertEquals(rs.get(0), new Range(1,20));
 	}
 
 	@Test
 	public void testShouldMergeAdjacentRanges() {
 		rs.add(16, 18);
+		assertEquals(rs.length(), 3);
 		assertEquals(rs.get(0), new Range(1,3));
 		assertEquals(rs.get(1), new Range(5,5));
 		assertEquals(rs.get(2), new Range(10,18));
@@ -68,6 +73,7 @@ public class FilledRangeSetTest {
 	@Test
 	public void testShouldMergeTwoAdjacentRanges() {
 		rs.add(4, 4);
+		assertEquals(rs.length(), 2);
 		assertEquals(rs.get(0), new Range(1,5));
 		assertEquals(rs.get(1), new Range(10,15));
 	}
