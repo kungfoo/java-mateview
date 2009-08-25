9890fd5d3fbdcec4adca8e474178fe3dbe08455a
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-08-25 17:24:57 +0200

    code massage. some refactoring.

--- src/com/redcareditor/mate/Bundle.java
+++ src/com/redcareditor/mate/Bundle.java
@@ -1,4 +1,3 @@
-
 package com.redcareditor.mate;
 
 import java.io.File;
@@ -11,7 +10,7 @@ public class Bundle {
 	public String name;
 	public List<Grammar> grammars;
 	public static List<Bundle> bundles;
-	
+
 	public Bundle(String name) {
 		this.name = name;
 		this.grammars = new ArrayList<Grammar>();
@@ -24,14 +23,15 @@ public class Bundle {
 		}
 		return null;
 	}
-	
-	// Return a list of bundle names like "Ruby.tmbundle"
+
+	/**
+	 * Return a list of bundle names like "Ruby.tmbundle"
+	 */
 	public static List<String> bundleDirs(String textmateDir) {
 		List<String> names = new ArrayList<String>();
-		File dir = new File(textmateDir+"/Bundles");
-		String[] children = dir.list();
-		if (children != null) {
-			for (String name : children) {
+		File dir = new File(textmateDir + "/Bundles");
+		if (dir.exists()) {
+			for (String name : dir.list()) {
 				if (name.endsWith(".tmbundle")) {
 					names.add(name);
 				}
@@ -39,36 +39,46 @@ public class Bundle {
 		}
 		return names;
 	}
-	
+
 	public static void loadBundles(String textmateDir) {
-		if (bundles != null)
+		if (bundles != null) {
 			return;
+		}
 		bundles = new ArrayList<Bundle>();
 		for (String bundleDir : bundleDirs(textmateDir)) {
 			Bundle bundle = new Bundle(bundleDir.split("\\.")[0]);
 			bundles.add(bundle);
 			File syntaxDir = new File(textmateDir + "/Bundles/" + bundleDir + "/Syntaxes");
 			if (syntaxDir.exists()) {
-				String[] children = syntaxDir.list();
-				for (String syntaxFileName : children) {
-					if (syntaxFileName != ".svn" &&
-							(syntaxFileName.endsWith(".tmLanguage") || 
-									syntaxFileName.endsWith(".plist"))
-					) {
-						Dict plist = Dict.parseFile(syntaxDir.getPath() + "/" + syntaxFileName);
-						if (plist != null) {
-							Grammar grammar = new Grammar(plist);
-							grammar.fileName = syntaxFileName;
-							bundle.grammars.add(grammar);
-						}
-					}
+				loadSyntax(bundle, syntaxDir);
+			}
+		}
+
+		for (Bundle b : bundles) {
+			for (Grammar g : b.grammars) {
+				g.initForReference();
+			}
+		}
+	}
+
+	private static void loadSyntax(Bundle bundle, File syntaxDir) {
+		for (String syntaxFileName : syntaxDir.list()) {
+			if (isNoSvnDir(syntaxFileName) && isTmBundlefile(syntaxFileName)) {
+				Dict plist = Dict.parseFile(syntaxDir.getPath() + "/" + syntaxFileName);
+				if (plist != null) {
+					Grammar grammar = new Grammar(plist);
+					grammar.fileName = syntaxFileName;
+					bundle.grammars.add(grammar);
 				}
 			}
 		}
-		
-		for (Bundle b : bundles)
-			for (Grammar g : b.grammars)
-				g.initForReference();
-		return;
+	}
+
+	private static boolean isTmBundlefile(String syntaxFileName) {
+		return (syntaxFileName.endsWith(".tmLanguage") || syntaxFileName.endsWith(".plist"));
+	}
+
+	private static boolean isNoSvnDir(String syntaxFileName) {
+		return syntaxFileName != ".svn";
 	}
 }
