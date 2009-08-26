e0326938a4fb13ea1c140b6b070539e5d9f26f98
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-26 06:19:43 +0100

    Played with StyledText listeners and added ThemeManager.loadThemes

--- src/com/redcareditor/mate/MateText.java
+++ src/com/redcareditor/mate/MateText.java
@@ -1,10 +1,45 @@
 package com.redcareditor.mate;
 
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.widgets.Composite;
 
 public class MateText extends StyledText {
+	private int modifyStart, modifyEnd;
+	private String modifyText;
+	
 	public MateText(Composite composite, int style) {
 		super(composite, style);
+		addListeners();
+	}
+	
+	public void addListeners() {
+		addVerifyListener(new VerifyListener() {
+			public void verifyText(VerifyEvent e) {
+				verifyEventCallback(e.start, e.end, e.text);
+			}
+		});
+		
+		addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				modifyEventCallback();
+			}
+		});
+	}
+	
+	public void verifyEventCallback(int start, int end, String text) {
+		modifyStart = start;
+		modifyEnd   = end;
+		modifyText  = text;
+	}
+	
+	public void modifyEventCallback() {
+		System.out.printf("modifying %d - %d, %d, %s\n", modifyStart, modifyEnd, getLineAtOffset(modifyStart), modifyText);
 	}
 }
+
+
+
--- src/com/redcareditor/theme/ThemeManager.java
+++ src/com/redcareditor/theme/ThemeManager.java
@@ -1,51 +1,76 @@
 package com.redcareditor.theme;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.ReentrantLock;
+
+import com.redcareditor.plist.Dict;
 
 /**
  * Thread-safe class that will load the themes if necessary.
  */
 public class ThemeManager {
-	private static Lock mutex;
-	private static List<Theme> themes;
+//	private static Lock mutex;
+	public static List<Theme> themes;
+//
+//	static {
+//		mutex = new ReentrantLock();
+//	}
+//
+//	/**
+//	 * loads (if necessary) and return the themes currently deployed for this.
+//	 */
+//	public static List<Theme> getThemes() {
+//		try {
+//			mutex.lock();
+//			themes = new ArrayList<Theme>();
+//			if (!initialized()) {
+//				loadThemes();
+//			}
+//			mutex.unlock();
+//		} finally {
+//			mutex.unlock();
+//		}
+//		return themes;
+//	}
+//
+//	public static void addTheme(Theme theme) {
+//		try {
+//			mutex.lock();
+//			themes.add(theme);
+//			mutex.unlock();
+//		} finally {
+//			mutex.unlock();
+//		}
+//	}
 
-	static {
-		mutex = new ReentrantLock();
-		loadThemes();
-	}
-
-	/**
-	 * loads (if necessary) and return the themes currently deployed for this.
-	 */
-	public static List<Theme> getThemes() {
-		try {
-			mutex.lock();
-			themes = new ArrayList<Theme>();
-			if (!initialized()) {
-				loadThemes();
+	public static List<String> themeNames(String textmateDir) {
+		List<String> names = new ArrayList<String>();
+		File dir = new File(textmateDir + "/Themes");
+		if (dir.exists()) {
+			for (String name : dir.list()) {
+				if (name.endsWith(".tmTheme")) {
+					names.add(name);
+				}
 			}
-			mutex.unlock();
-		} finally {
-			mutex.unlock();
 		}
-		return themes;
+		return names;
 	}
-
-	public static void addTheme(Theme theme) {
-		try {
-			mutex.lock();
-			themes.add(theme);
-			mutex.unlock();
-		} finally {
-			mutex.unlock();
+	
+	public static void loadThemes(String textmateDir) {
+		if (themes != null) {
+			return;
 		}
-	}
-
-	private static void loadThemes() {
-		// TODO: implement loading all themes from the appropriate directory.
+		themes = new ArrayList<Theme>();
+		for (String themeName : themeNames(textmateDir)) {
+			Dict dict = Dict.parseFile(textmateDir + "/Themes/" + themeName);
+			if (dict != null) {
+				Theme theme = new Theme(dict);
+				themes.add(theme);
+			}
+		}
 	}
 
 	private static boolean initialized() {
