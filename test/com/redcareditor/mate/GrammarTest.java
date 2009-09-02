0f5c1f8a02a9d94f9f82a2ff0689de682edb6866
Mat Schaffer <mat.schaffer@gmail.com>
2009-09-02 09:26:58 -0400

    Porting more tests over from gtkmateview's grammar_spec.rb.

--- test/com/redcareditor/mate/GrammarTest.java
+++ test/com/redcareditor/mate/GrammarTest.java
@@ -3,6 +3,7 @@ package com.redcareditor.mate;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,36 +26,61 @@ public class GrammarTest {
 		assertEquals("Apache", g.name);
 		assertEquals("source.apache-config", g.scopeName);
 	}
-	
-	public ArrayList<String> patternNames() {
-		ArrayList<String> result = new ArrayList<String>();
-		for (Pattern p : g.allPatterns) {
-			result.add(p.name);
-//			System.out.printf("test name: %s\n", p.name);
-		}
-		return result;
-	}
-	
+
 	@Test
 	public void shouldLoadPatternsIntoMemory() {
 		assertTrue("allPatterns is not empty", g.allPatterns.size() > 0);
-		assertTrue(patternNames().contains("comment.line.number-sign.apache-config"));
-		assertTrue(patternNames().contains("source.include.apache-config"));
-		assertTrue(patternNames().contains("support.constant.apache-config"));
+		List<String> patternNames = patternNames(g.allPatterns);
+		assertTrue(patternNames.contains("comment.line.number-sign.apache-config"));
+		assertTrue(patternNames.contains("source.include.apache-config"));
+		assertTrue(patternNames.contains("support.constant.apache-config"));
 	}
 
   @Test
   public void shouldLoadPatternsWithoutNames() {
-    Pattern foundPattern = null;
+    Pattern foundPattern = find(g.allPatterns, new Predicate() {
+      public boolean match(Pattern p) {
+        return (p instanceof DoublePattern) &&
+               ((DoublePattern)p).bothCaptures != null &&
+               ((DoublePattern)p).bothCaptures.values().contains("support.constant.rewritecond.apache-config");
+      }
+    });
+    assertNotNull("Unable to find unnamed rewrite pattern", foundPattern);
+  }
+
+  public void assertNotIncludePattern(Pattern p) {
+    assertFalse(p instanceof IncludePattern);
+  }
+
+  @Test
+  public void shouldReplaceAllIncludePatterns() {
     for (Pattern p : g.allPatterns) {
-      if ((p instanceof DoublePattern) &&
-          ((DoublePattern)p).bothCaptures != null &&
-          ((DoublePattern)p).bothCaptures.values().contains("support.constant.rewritecond.apache-config")) {
-        foundPattern = p;
-        break;
+      assertNotIncludePattern(p);
+      if (p instanceof DoublePattern) {
+        for (Pattern p1 : ((DoublePattern)p).patterns) {
+          assertNotIncludePattern(p1);
+          if (p1 instanceof DoublePattern) {
+            for (Pattern p2 : ((DoublePattern)p1).patterns)
+            assertNotIncludePattern(p2);
+          }
+        }
       }
     }
-    assertNotNull("Unable to find unnamed rewrite pattern", foundPattern);
+  }
+
+  @Test
+  public void shouldReplacePatternBaseProperly() {
+    Pattern p = find(g.allPatterns, new Predicate() {
+      public boolean match(Pattern p) {
+        return "meta.vhost.apache-config".equals(p.name);
+      }
+    });
+    if (p instanceof DoublePattern) {
+      List<String> patternNames = patternNames(((DoublePattern)p).patterns);
+      assertTrue(patternNames.contains("meta.vhost.apache-config"));
+    } else {
+      fail("Expected \"meta.vhost.apache-config\" to parse as a DoublePattern.");
+    }
   }
 
 	@Test
@@ -65,4 +91,25 @@ public class GrammarTest {
 			}
 		}
 	}
+
+  public ArrayList<String> patternNames(List<Pattern> patterns) {
+    ArrayList<String> result = new ArrayList<String>();
+    for (Pattern p : patterns) {
+      result.add(p.name);
+    }
+    return result;
+  }
+
+  public Pattern find(List<Pattern> patterns, Predicate pred) {
+    for (Pattern p : patterns) {
+      if (pred.match(p)) {
+        return p;
+      }
+    }
+    return null;
+  }
+
+  interface Predicate {
+    public boolean match(Pattern p);
+  }
 }
