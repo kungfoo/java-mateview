2d6875ce547d35348a056904e3f619d3ee7a1b79
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-09-04 18:53:36 +0200

    great. the test fails.

--- test/com/redcareditor/mate/MateTextUndoRedoTest.java
+++ test/com/redcareditor/mate/MateTextUndoRedoTest.java
@@ -1,5 +1,8 @@
 package com.redcareditor.mate;
 
+import static org.junit.Assert.*;
+
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Shell;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,15 +10,22 @@ import org.junit.Test;
 
 public class MateTextUndoRedoTest {
 	private MateText mateText;
+	private StyledText text;
 
 	@Before
 	public void setup(){
 		Shell shell = new Shell();
 		mateText = new MateText(new MateText(shell));
+		text = mateText.getTextWidget();
 	}
 	
 	@Test
 	public void testOneStepUndoRedo(){
-		
+		String string = "Hey, I entered this text just now!";
+		text.replaceTextRange(0, 0, string);
+		mateText.undo();
+		assertEquals("", text.getText());
+		mateText.redo();
+		assertEquals(string, text.getText());
 	}
 }
