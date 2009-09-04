7b28b9dcd64ea6240d0401509a8edaf0c5c0414f
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-09-04 17:24:16 +0200

    working example app. Nice, though the menu doesn't show yet...

--- lib/swt_wrapper.rb
+++ lib/swt_wrapper.rb
@@ -6,6 +6,7 @@ module Swt
   module Widgets
     import org.eclipse.swt.widgets.Display
     import org.eclipse.swt.widgets.Shell
+    import org.eclipse.swt.widgets.Composite
     import org.eclipse.swt.widgets.Menu
     import org.eclipse.swt.widgets.MenuItem
   end
