a3ad851d38964bac15b80b4954bd906ebf3d7595
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-09-05 08:50:40 +0200

    formatting and spelling.

--- src/com/redcareditor/mate/document/MateTextLocationComparator.java
+++ src/com/redcareditor/mate/document/MateTextLocationComparator.java
@@ -2,15 +2,15 @@ package com.redcareditor.mate.document;
 
 import java.util.Comparator;
 
-public class MateTextLocationComperator implements Comparator<MateTextLocation> {
+public class MateTextLocationComparator implements Comparator<MateTextLocation> {
 
 	public int compare(MateTextLocation arg0, MateTextLocation arg1) {
 		int lineCompare = arg0.getLine() - arg1.getLine();
-		
-		if(lineCompare == 0){
+
+		if (lineCompare == 0) {
 			return arg0.getLineOffset() - arg1.getLineOffset();
 		}
-		
+
 		return lineCompare;
 	}
 
--- src/com/redcareditor/mate/document/swt/SwtTextLocation.java
+++ src/com/redcareditor/mate/document/swt/SwtTextLocation.java
@@ -4,44 +4,37 @@ import org.eclipse.jface.text.Position;
 import org.eclipse.swt.custom.StyledText;
 
 import com.redcareditor.mate.document.MateTextLocation;
-import com.redcareditor.mate.document.MateTextLocationComperator;
+import com.redcareditor.mate.document.MateTextLocationComparator;
 
 public class SwtTextLocation extends Position implements MateTextLocation {
-	
-	private static final MateTextLocationComperator comperator = new MateTextLocationComperator();
-	
-	SwtMateDocument document;
-	
-	public SwtTextLocation(int line, int lineOffset, SwtMateDocument document){
+
+	private static final MateTextLocationComparator comperator = new MateTextLocationComparator();
+	private SwtMateDocument document;
+
+	public SwtTextLocation(int line, int lineOffset, SwtMateDocument document) {
 		super(computeOffset(line, lineOffset, document.styledText));
-		
 		this.document = document;
 	}
-	
-	public SwtTextLocation(MateTextLocation location, SwtMateDocument document){
-		super(computeOffset(
-				location.getLine(), 
-				location.getLineOffset(), 
-				document.styledText));
-		
+
+	public SwtTextLocation(MateTextLocation location, SwtMateDocument document) {
+		super(computeOffset(location.getLine(), location.getLineOffset(), document.styledText));
 		this.document = document;
 	}
-	
+
 	public int getLine() {
 		return document.styledText.getLineAtOffset(getOffset());
 	}
 
 	public int getLineOffset() {
-		
 		return offset - document.styledText.getOffsetAtLine(getLine());
 	}
 
 	public int compareTo(MateTextLocation o) {
 		return comperator.compare(this, o);
 	}
-	
-	private static int computeOffset(int line, int offset, StyledText text){
-		return text.getOffsetAtLine(line)+offset;
+
+	private static int computeOffset(int line, int offset, StyledText text) {
+		return text.getOffsetAtLine(line) + offset;
 	}
 
 }
